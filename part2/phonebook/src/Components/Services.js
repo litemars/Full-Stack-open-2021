f4407e44cf40b4c1dd50575a20c9dab248d12a92
litemars <maxmassi12@gmail.com>
2021-09-27 14:22:31 +0300

    host

--- part2/phonebook/src/Components/Services.js
+++ part2/phonebook/src/Components/Services.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-//const baseUrl = 'https://nodejs-aalto.herokuapp.com/api/persons'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://nodejs-aalto.herokuapp.com/api/persons'
+//const baseUrl = 'http://localhost:3001/api/persons'
 export const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
